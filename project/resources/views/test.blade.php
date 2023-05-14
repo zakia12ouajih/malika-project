6f6959cf3e7a0abe64f7b4bac57655b400d9ec19
imane <idmom4914@gmail.com>
2023-05-14 17:31:28 +0200

    p

--- project/resources/views/test.blade.php
+++ project/resources/views/test.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+@vite('resources/sass/app.scss','resourcse/js/app.js')
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
@@ -301,7 +302,7 @@ footer{
                     <img src="les-image/photo2.jpg" height="220px" style="padding-bottom: 30px" >
                     {{-- <p >{{ $s->nom }}</p> --}}
                     <p>Les information</p>
-                    
+
                 </div>
             </div>
             {{-- @endforeach --}}
